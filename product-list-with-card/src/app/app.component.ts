d372481dbd7367331310ba922bbfc366eb681fba
Dave Henson <david.henson@bootsdigitalhealth.co.uk>
2024-10-30 21:09:59 +0000

    Add top level components

--- product-list-with-card/src/app/app.component.ts
+++ product-list-with-card/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsComponent, CartComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'product-list-with-card';
+  title = 'Deserts';
 }
